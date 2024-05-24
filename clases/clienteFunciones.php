5151b3b7f754abbf9aca780bd1099e6230f9ccaf
davidf110102 <davidfloresalazar11@gmail.com>
2024-05-24 01:02:13 -0500

    validaciones de login e implentacion a travez de un boton en el header

--- clases/clienteFunciones.php
+++ clases/clienteFunciones.php
@@ -72,3 +72,31 @@ function mostrarMensajes(array $errors){
         echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
     }
 }
+
+function login($usuario, $password, $con){
+    $sql = $con->prepare("SELECT id, usuario, password FROM usuarios WHERE usuario LIKE ? LIMIT 1");
+    $sql->execute([$usuario]);
+    if($row = $sql->fetch(PDO::FETCH_ASSOC)){
+        if(esActivo($usuario, $con)){
+            if(password_verify($password, $row['password'])){
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['user_name'] = $row['usuario'];
+                header("Location: index.php");
+                exit;
+            }
+        } else {
+            return "El usuario no ha sido activado.";
+        }
+    }
+    return 'El usuario y/o contraseña son incorrectos.';
+}
+
+function esActivo($usuario, $con){
+    $sql = $con->prepare("SELECT activacion FROM usuarios WHERE usuario LIKE ? LIMIT 1");
+    $sql->execute([$usuario]);
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+    if($row['activacion'] == 1){
+        return true;
+    }
+    return false;
+}
