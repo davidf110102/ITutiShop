6c318e0f860f182dc1b002f3e162fc49616a5fc7
KevinVichi <vichicelakevin@gmail.com>
2024-06-07 21:15:32 -0500

    Coneccion y correccion de actualizacion de la tabla configuracion

--- admin/configuracion/guarda.php
+++ admin/configuracion/guarda.php
@@ -3,16 +3,28 @@ require '../config/database.php';
 require '../config/config.php';
 require '../header.php';
 
+$db = new Database();
+$con = $db->conectar();
+
+$smtp = $_POST['smtp'];
+$puerto = $_POST['puerto'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = $con->prepare("UPDATE configuracion SET valor=? Where nombre=?");
+$sql->execute([$smtp, 'correo_smtp']);
+$sql->execute([$puerto, 'correo_puerto']);
+$sql->execute([$email, 'correo_email']);
+$sql->execute([$password, 'correo_password']);
 ?>
 <main>
   <div class="container-fluid px4">
-    <h1 class="mt-4">Dashboard</h1>
-    <ol class="breadcrumb mb-4">
-      <li class="breadcrumb-item active">Dashboard</li>
+    <h2 class="mt-4">Configuracion actualizada</h2>
+    <a href="index.php" class="btn btn-secondary">Regresar</a>
     </ol>
   </div>
 </main>
 
 
 
-<?php include 'footer.php' ?>
+<?php include '../footer.php' ?>
