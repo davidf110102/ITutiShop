54eae62b4e61546122fda25e6dd861849db7e181
KevinVichi <vichicelakevin@gmail.com>
2024-06-07 21:04:58 -0500

    Creacion guarda.php y configuracion en admin

---
+++ admin/configuracion/guarda.php
@@ -0,0 +1,18 @@
+<?php
+require '../config/database.php';
+require '../config/config.php';
+require '../header.php';
+
+?>
+<main>
+  <div class="container-fluid px4">
+    <h1 class="mt-4">Dashboard</h1>
+    <ol class="breadcrumb mb-4">
+      <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+  </div>
+</main>
+
+
+
+<?php include 'footer.php' ?>
