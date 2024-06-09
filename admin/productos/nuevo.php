8bc0f3278c3c7e4f7da66d8dfbed385261ca7e92
davidf110102 <davidfloresalazar11@gmail.com>
2024-06-08 19:48:20 -0500

    se añadio la carga de categorias

--- admin/productos/nuevo.php
+++ admin/productos/nuevo.php
@@ -16,11 +16,16 @@ if ($_SESSION['user_type'] != 'admin') {
 $db = new Database();
 $con = $db->conectar();
 
+$sql = "SELECT * FROM categorias WHERE activo = 1";
+$resultado = $con->query($sql);
+$categorias = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 ?>
 <main>
     <div class="container-fluid px-4">
-        <h2 class="mt-3">Nuevo Producto</h2>
+        <h2 class="mt-2">Nuevo Producto</h2>
 
         <form action="guarda.php" method="post" autocomplete="off">
             <div class="mb-3">
@@ -31,7 +36,7 @@ $con = $db->conectar();
                 <label for="descripcion" class="form-label">Descripción</label>
                 <textarea class="form-control" name="descripcion" id="descripcion" required></textarea>
             </div>
-            <div class="row"
+            <div class="row">
                 <div class="col mb-3">
                     <label for="precio" class="form-label">Precio</label>
                     <input type="number" class="form-control" name="precio" id="precio" required>
@@ -45,11 +50,14 @@ $con = $db->conectar();
                     <input type="number" class="form-control" name="stock" id="stock" required>
                 </div>
             </div>
-            <div class="row"
+            <div class="row">
                 <div class="col-4 mb-3">
                     <label for="categoria" class="form-label">Categoría</label>
                     <select class = "form-select" name="categoria" id="categoria" required>
                         <option value="">Seleccionar</option>
+                        <?php foreach($categorias as $categoria){?>
+                            <option value="<?php echo $categoria['id']; ?>"><?php echo $categoria['nombre']; ?></option>
+                        <?php } ?>
                     </select>
                 </div>
             </div>
