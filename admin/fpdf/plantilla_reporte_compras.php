21e5489e969a4686167d2f72a687cad819f7a8ba
KevinVichi <vichicelakevin@gmail.com>
2024-06-16 00:31:59 -0500

    correcion reporte compras

--- admin/fpdf/plantilla_reporte_compras.php
+++ admin/fpdf/plantilla_reporte_compras.php
@@ -1,1 +1,24 @@
 <?php
+
+require 'fpdf.php';
+
+class PDF extends FPDF
+{
+  private $fechaIni;
+  private $fechaFin;
+
+  public function __construct($orientacion, $medidas, $tamanio, $datos)
+  {
+    parent::__construct($orientacion, $medidas, $tamanio);
+    $this->fechaIni = $datos['fechaIni'];
+    $this->fechaFin = $datos['fechaFin'];
+  }
+
+  public function Header()
+  {
+    $this->Image('../images/logo.png', 10, 5, 20);
+    $this->SetFont('Arial', 'B', 11);
+
+    $this->MultiCell(130, 10, 'Reporte de compras', 1, 'C');
+  }
+}
