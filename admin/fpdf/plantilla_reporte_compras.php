7878c76173fbcb33440876f398eb68b063914ada
KevinVichi <vichicelakevin@gmail.com>
2024-06-16 00:47:09 -0500

    fecha de consulta para el reporte

--- admin/fpdf/plantilla_reporte_compras.php
+++ admin/fpdf/plantilla_reporte_compras.php
@@ -19,6 +19,14 @@ class PDF extends FPDF
     $this->Image('../images/logo.png', 10, 5, 20);
     $this->SetFont('Arial', 'B', 11);
 
-    $this->MultiCell(130, 10, 'Reporte de compras', 1, 'C');
+    $this->Cell(30);
+    $y = $this->GetY();
+    $this->MultiCell(130, 10, 'Reporte de compras', 0, 'C');
+
+    $this->SetXY(160, $y);
+    $this->SetFont('Arial', '', 11);
+    $this->Cell(40, 10, 'Fecha: ' . date('d/m/Y'), 0, 1, 'L');
+
+    $this->Ln(8);
   }
 }
